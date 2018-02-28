91395ae859ca3805d80bc16475a930b9f90f0c17
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-02-28 16:56:53 +0100

    Initial Commit

---
+++ 42IA/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  42IA
+//
+//  Created by Benjamin Pisano on 28/02/2018.
+//  Copyright © 2018 bpisano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
